2f6cf714d647a8dd03afd1bb2c275fb6f4f78ffd
adityabujji143 <adityabujji143@gmail.com>
2026-02-21 17:18:39 +0000

    update ideaRoutes.js

--- server/src/routes/ideaRoutes.js
+++ server/src/routes/ideaRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { createIdea, getFeed, getIdeaById, deleteIdea, likeIdea, bookmarkIdea, addComment, expressInterest, getIdeaInterests, searchIdeas } = require('../controllers/ideaController');
+const { createIdea, getFeed, getIdeaById, updateIdea, deleteIdea, likeIdea, bookmarkIdea, addComment, expressInterest, getIdeaInterests, searchIdeas } = require('../controllers/ideaController');
 const { authenticate, optionalAuth } = require('../middleware/auth');
 const { attachTierLimits } = require('../middleware/tierCheck');
 const upload = require('../middleware/upload');
@@ -9,6 +9,7 @@ router.get('/search', optionalAuth, searchIdeas);
 router.get('/feed', optionalAuth, getFeed);
 router.get('/:id', optionalAuth, getIdeaById);
 router.post('/', authenticate, attachTierLimits, upload.array('files', 10), createIdea);
+router.put('/:id', authenticate, attachTierLimits, updateIdea);
 router.delete('/:id', authenticate, deleteIdea);
 router.post('/:id/like', authenticate, likeIdea);
 router.post('/:id/bookmark', authenticate, bookmarkIdea);
